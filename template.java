d5ef6934979c90f7ecec7c8672eba25d3d72bb28
Ushen Wijayaratne <108585387+its-ushen@users.noreply.github.com>
2023-10-04 11:31:54 +0100

    Create template.java

---
+++ template.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void add(int num1, int num2) {
+
+    }
+
+    public static void multiply(int num1, int num2) {
+
+    }
+}
