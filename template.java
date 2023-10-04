d316fd371f89d06cdd91a87a7a33f136b220fdb7
DaraHeaphy <daraheaphy@gmail.com>
2023-10-04 12:13:58 +0100

    AdditionBranch

--- template.java
+++ template.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args) {
-        
+
+        System.out.println("Your answer is " + Integer.toString(add(1, 2)));
     }
 
-    public static void add(int num1, int num2) {
+    public static int add(int num1, int num2) {
+        {
+            int result = 0;
+            switch (operation) {
+                case "+":
+                result = num1 + num2;
+            }
+        }
+
+        
 
     }
 
