33ef4027b45376a39b1c614a4a14fb57cdafb3a5
DaraHeaphy <daraheaphy@gmail.com>
2023-10-04 12:32:03 +0100

    trial

--- template.java
+++ template.java
@@ -3,20 +3,10 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
 
-        System.out.println("Your answer is " + Integer.toString(add(1, 2)));
     }
 
     public static int add(int num1, int num2) {
-        {
-            int result = 0;
-            switch (operation) {
-                case "+":
-                result = num1 + num2;
-            }
-        }
-
-        
-
+        return num1 + num2;
     }
 
     public static void multiply(int num1, int num2) {
